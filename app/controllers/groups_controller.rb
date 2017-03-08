f10d4c9c843b11e2f7807019fc0572263ca8123d
盧柏嘉 <lupochia@LuPaul.local>
2017-03-08 16:12:25 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -23,17 +24,19 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    # find_group_and_check_permission
     @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission"
-    end
+    # if current_user != @group.user
+    #   redirect_to root_path, alert: "You have no permission"
+    # end
   end
 
   def update
+    # find_group_and_check_permission
     @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission"
-    end
+    # if current_user != @group.user
+    #   redirect_to root_path, alert: "You have no permission"
+    # end
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -42,10 +45,11 @@ class GroupsController < ApplicationController
   end
 
   def destroy
+    # find_group_and_check_permission
     @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission"
-    end
+    # if current_user != @group.user
+    #   redirect_to root_path, alert: "You have no permission"
+    # end
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
@@ -53,6 +57,13 @@ class GroupsController < ApplicationController
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to groups_path, alert: "You have no permission!"
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
