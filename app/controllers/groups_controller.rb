2f850f403bf9d255fab810eb3d8e2082f827dfdf
盧柏嘉 <lupochia@LuPaul.local>
2017-03-08 14:41:57 +0800

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,12 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  # def create
+  #   @gorup = Group.new()
+  # end
 end
